b935e841296187ac54139e85937444925adf5159
Kirubelsol <84950526+Kirubelsol@users.noreply.github.com>
2021-06-02 08:43:53 +0400

    Update temperature_main.ino
    temperature graph edited

--- temperature_main/temperature_main.ino
+++ temperature_main/temperature_main.ino
@@ -47,5 +47,199 @@ uint8_t color = 0; // the state of the color
 
 void loop()
 {
+  long currentTime = millis();
+ 
 
+  if (IMU6886Flag == true)
+  {
+
+    M5.IMU.getTempData(&temp); //get the temperature
+
+    if (currentTime - previoustime ==  timeinterval1) {
+      temp1 = temp;
+      Serial.printf("%.2f Temp1", temp1);
+
+    }
+    if (currentTime - previoustime == timeinterval2) {
+      temp2 = temp;
+      Serial.printf("%.2f Temp2", temp2);
+    }
+    if (currentTime - previoustime == timeinterval3) {
+      temp3 = temp;
+      Serial.printf("%.2f Temp3", temp3);
+    }
+    if (currentTime - previoustime == timeinterval4) {
+      temp4 = temp;
+      Serial.printf("%.2f Temp4", temp4);
+    }
+    if (currentTime - previoustime == timeinterval5) {
+      temp5 = temp;
+      Serial.printf("%.2f Temp5", temp5);
+    }
+    // for the first column pixels
+    if (temp1 < 25 && temp1 > 1) {
+      M5.dis.drawpix(20, 0xff0000);
+      Serial.printf("Y");
+    }
+    if (temp1 >= 25 && temp1 < 30) {
+
+      M5.dis.drawpix(15, 0xff0000);
+      M5.dis.drawpix(20, 0xff0000);
+    }
+    if (temp1 >= 30 && temp1 < 35) {
+      M5.dis.drawpix(10, 0xff0000);
+      M5.dis.drawpix(15, 0xff0000);
+      M5.dis.drawpix(20, 0xff0000);
+    }
+    if (temp1 >= 35 && temp1 < 40) {
+      M5.dis.drawpix(5, 0xff0000);
+      M5.dis.drawpix(10, 0xff0000);
+      M5.dis.drawpix(15, 0xff0000);
+      M5.dis.drawpix(20, 0xff0000);
+    }
+    if (temp1 >= 40) {
+      M5.dis.drawpix(0, 0xff0000);
+      M5.dis.drawpix(5, 0xff0000);
+      M5.dis.drawpix(10, 0xff0000);
+      M5.dis.drawpix(15, 0xff0000);
+      M5.dis.drawpix(20, 0xff0000);
+    }
+    ////////
+
+    // for the second column pixels
+    if (temp2 < 25 && temp2 > 1) {
+      M5.dis.drawpix(21, 0xff0000);
+    }
+    if (temp2 >= 25 && temp2 < 30) {
+      M5.dis.drawpix(16, 0xff0000);
+      M5.dis.drawpix(21, 0xff0000);
+    }
+    if (temp2 >= 30 && temp2 < 35) {
+      M5.dis.drawpix(11, 0xff0000);
+      M5.dis.drawpix(16, 0xff0000);
+      M5.dis.drawpix(21, 0xff0000);
+    }
+    if (temp2 >= 35 && temp2 < 40) {
+      M5.dis.drawpix(6, 0xff0000);
+      M5.dis.drawpix(11, 0xff0000);
+      M5.dis.drawpix(16, 0xff0000);
+      M5.dis.drawpix(21, 0xff0000);
+    }
+    if (temp2 >= 40) {
+      M5.dis.drawpix(1, 0xff0000);
+      M5.dis.drawpix(6, 0xff0000);
+      M5.dis.drawpix(11, 0xff0000);
+      M5.dis.drawpix(16, 0xff0000);
+      M5.dis.drawpix(21, 0xff0000);
+    }
+    ////////
+
+    // for the third column pixels
+    if (temp3 < 25 && temp3 > 1) {
+      M5.dis.drawpix(22, 0xff0000);
+    }
+    if (temp3 >= 25 && temp3 < 30) {
+      M5.dis.drawpix(17, 0xff0000);
+      M5.dis.drawpix(22, 0xff0000);
+    }
+    if (temp3 >= 30 && temp3 < 35) {
+      M5.dis.drawpix(12, 0xff0000);
+      M5.dis.drawpix(15, 0xff0000);
+      M5.dis.drawpix(22, 0xff0000);
+    }
+    if (temp3 >= 35 && temp3 < 40) {
+      M5.dis.drawpix(7, 0xff0000);
+      M5.dis.drawpix(12, 0xff0000);
+      M5.dis.drawpix(17, 0xff0000);
+      M5.dis.drawpix(22, 0xff0000);
+    }
+    if (temp3 >= 40) {
+      M5.dis.drawpix(2, 0xff0000);
+      M5.dis.drawpix(7, 0xff0000);
+      M5.dis.drawpix(12, 0xff0000);
+      M5.dis.drawpix(17, 0xff0000);
+      M5.dis.drawpix(22, 0xff0000);
+    }
+    ////////
+
+    // for the forth column pixels
+    if (temp4 < 25 && temp4 > 1) {
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    if (temp4 >= 25 && temp4 < 30) {
+      M5.dis.drawpix(18, 0xff0000);
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    if (temp4 >= 30 && temp4 < 35) {
+      M5.dis.drawpix(13, 0xff0000);
+      M5.dis.drawpix(18, 0xff0000);
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    if (temp4 >= 35 && temp4 < 40) {
+      M5.dis.drawpix(8, 0xff0000);
+      M5.dis.drawpix(13, 0xff0000);
+      M5.dis.drawpix(18, 0xff0000);
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    if (temp4 >= 40) {
+      M5.dis.drawpix(3, 0xff0000);
+      M5.dis.drawpix(8, 0xff0000);
+      M5.dis.drawpix(13, 0xff0000);
+      M5.dis.drawpix(18, 0xff0000);
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    ////////
+
+
+    // for the fifth column pixels
+    if (temp5 < 25 && temp5 > 1) {
+      M5.dis.drawpix(24, 0xff0000);
+    }
+    if (temp5 >= 25 && temp4 < 30) {
+      M5.dis.drawpix(19, 0xff0000);
+      M5.dis.drawpix(23, 0xff0000);
+    }
+    if (temp5 >= 30 && temp4 < 35) {
+      M5.dis.drawpix(14, 0xff0000);
+      M5.dis.drawpix(19, 0xff0000);
+      M5.dis.drawpix(24, 0xff0000);
+    }
+    if (temp5 >= 35 && temp4 < 40) {
+      M5.dis.drawpix(9, 0xff0000);
+      M5.dis.drawpix(14, 0xff0000);
+      M5.dis.drawpix(19, 0xff0000);
+      M5.dis.drawpix(24, 0xff0000);
+    }
+    if (temp5 >= 40) {
+      M5.dis.drawpix(4, 0xff0000);
+      M5.dis.drawpix(9, 0xff0000);
+      M5.dis.drawpix(14, 0xff0000);
+      M5.dis.drawpix(19, 0xff0000);
+      M5.dis.drawpix(24, 0xff0000);
+    }
+    ////////
+
+    if (currentTime - previoustime == 30000) {
+      previoustime = currentTime;
+      temp1 = 0;
+      temp2 = 0;
+      temp3 = 0;
+      temp4 = 0;
+      temp5 = 0;
+      setBuff(0x00, 0x00, 0x00);
+      M5.dis.displaybuff(DisBuff);
+      M5.update();
+
+    };
+
+
+  }
+  else
+  {
+    // Fail to initialize IMU (Maybe)
+    M5.dis.clear();
+    M5.dis.drawpix(2, 2, CRGB::Red);
+    delay(500);
+    M5.update();
+  }
 }
