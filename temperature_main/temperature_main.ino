2c7e311bf06b813804631826e33d156651083c14
MagdalenaAlicja <84934379+MagdalenaAlicja@users.noreply.github.com>
2021-06-02 11:01:41 +0400

    Update temperature_main.ino
    forgot to add it to GitHub before. tilting

--- temperature_main/temperature_main.ino
+++ temperature_main/temperature_main.ino
@@ -47,5 +47,108 @@ uint8_t color = 0; // the state of the color
 
 void loop()
 {
+ if (currentTime - previoustime >= time_interval) {
+      previoustime = currentTime;
 
-}
+
+      if ((gyroY ) > 60 && (gyroY) < 300) {
+        counter++;
+
+      }
+
+
+      switch (counter)
+      {
+
+        case 1:
+
+          M5.dis.clear();
+          M5.dis.drawpix(3, 0xf00000);
+          M5.dis.drawpix(7, 0xf00000);
+          M5.dis.drawpix(8, 0xf00000);
+          M5.dis.drawpix(13, 0xf00000);
+          M5.dis.drawpix(18, 0xf00000);
+          M5.dis.drawpix(23, 0xf00000);
+
+          break;
+        case 2:
+
+         
+          M5.dis.clear();
+          M5.dis.drawpix(2, 0xf00000);
+          M5.dis.drawpix(3, 0xf00000);
+          M5.dis.drawpix(6, 0xf00000);
+          M5.dis.drawpix(8, 0xf00000);
+          M5.dis.drawpix(12, 0xf00000);
+          M5.dis.drawpix(16, 0xf00000);
+          M5.dis.drawpix(21, 0xf00000);
+          M5.dis.drawpix(22, 0xf00000);
+          M5.dis.drawpix(23, 0xf00000);
+
+          break;
+
+        case 3:
+
+            M5.dis.clear();
+            M5.dis.drawpix(1, 0xf00000);
+            M5.dis.drawpix(2, 0xf00000);
+            M5.dis.drawpix(3, 0xf00000);
+            M5.dis.drawpix(8, 0xf00000);
+            M5.dis.drawpix(11, 0xf00000);
+            M5.dis.drawpix(12, 0xf00000);
+            M5.dis.drawpix(13, 0xf00000);
+            M5.dis.drawpix(18, 0xf00000);
+            M5.dis.drawpix(21, 0xf00000);
+            M5.dis.drawpix(22, 0xf00000);
+            M5.dis.drawpix(23, 0xf00000); 
+          break;
+
+        case 4:
+
+          M5.dis.clear();
+          M5.dis.drawpix(2, 0xf00000);
+          M5.dis.drawpix(6, 0xf00000);
+          M5.dis.drawpix(10, 0xf00000);
+          M5.dis.drawpix(11, 0xf00000);
+          M5.dis.drawpix(12, 0xf00000);
+          M5.dis.drawpix(13, 0xf00000);
+          // M5.dis.drawpix(16, 0xf00000);
+          M5.dis.drawpix(17, 0xf00000);
+          M5.dis.drawpix(22, 0xf00000);
+          break;
+
+        case 5:
+
+          M5.dis.clear();
+          M5.dis.drawpix(1, 0xf00000);
+          M5.dis.drawpix(2, 0xf00000);
+          M5.dis.drawpix(3, 0xf00000);
+          M5.dis.drawpix(6, 0xf00000);
+          M5.dis.drawpix(11, 0xf00000);
+          M5.dis.drawpix(12, 0xf00000);
+          M5.dis.drawpix(13, 0xf00000);
+          M5.dis.drawpix(18, 0xf00000);
+          M5.dis.drawpix(21, 0xf00000);
+          M5.dis.drawpix(22, 0xf00000);
+          M5.dis.drawpix(23, 0xf00000);
+          M5.update();
+
+          break;
+      }
+
+
+      if ((gyroY) > 300) {
+        setBuff(0x00, 0x00, 0x00);
+        M5.dis.displaybuff(DisBuff); //add M5.update later
+        counter = 0;
+
+        M5.update();
+      }
+     
+      if (counter == 6) {
+        counter = 1;
+      }
+
+    }
+
+  }
